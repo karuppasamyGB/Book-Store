61aa25716d0577dafc21417fa14dad1ed860bf63
karuppasamyGB <148546076+karuppasamyGB@users.noreply.github.com>
2024-08-23 19:35:39 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -299,7 +299,7 @@ const Home = () => {
     useEffect(() => {
         setLoading(true);
         axios
-            .get('http://localhost:5555/books')
+            .get('https://book-server-malg.onrender.com/books')
             .then((response) => {
                 setBooks(response.data.data);
                 setLoading(false);
